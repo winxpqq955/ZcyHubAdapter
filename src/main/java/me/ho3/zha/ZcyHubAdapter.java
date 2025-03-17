10e3c4aecfa1d04356cf63067b19b76351e47d7e
ImashinoMisaki <ImashinoMisaki@aol.com>
2025-03-18 00:26:16 +0800

    Initial commit

---
+++ src/main/java/me/ho3/zha/ZcyHubAdapter.java
@@ -0,0 +1,17 @@
+package me.ho3.zcyHubAdapter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZcyHubAdapter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
